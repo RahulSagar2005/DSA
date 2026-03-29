f4b774c4be1937c4dece90089708a2807d92caae
Rahul Sagar <rahulsagarxgr2@gmail.com>
2026-03-29 12:09:26 +0530

    Moved files to recursion folder

---
+++ recursion/regular-expression-matching.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+    int t[21][21];
+    bool solve(int i,int j,string &s, string &p){
+        if(j==p.length()){
+            return i==s.length();
+        }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
+        bool firstcharmatch=false;
+        if(i<s.length() && (p[j]==s[i]|| p[j]=='.')){
+            firstcharmatch=true;
+        }
+        if(j+1<p.length() && p[j+1]=='*'){
+            bool nottake=solve(i,j+2,s,p);
+            bool take=firstcharmatch && solve(i+1,j,s,p);
+            return t[i][j]=nottake|| take;
+        }
+        return t[i][j]=firstcharmatch && solve(i+1,j+1,s,p);
+
+    }
+    bool isMatch(string s, string p) {
+        int i=0,j=0;
+        memset(t,-1,sizeof(t));
+        return solve(i,j,s,p);   
+    }
