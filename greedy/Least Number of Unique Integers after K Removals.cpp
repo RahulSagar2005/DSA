0365ca027d12f2b7f8090f567a6faa4a27aac3d6
Rahul Sagar <rahulsagarxgr2@gmail.com>
2026-04-24 19:59:59 +0530

    practise

--- greedy/Least Number of Unique Integers after K Removals.cpp
+++ greedy/Least Number of Unique Integers after K Removals.cpp
@@ -22,3 +22,29 @@ public:
         return 0;
     }
 };
+// second approach 
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        int n=arr.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        } 
+        int uniquetype=mp.size();
+        vector<int> freqcount(n+1,0);
+        for(auto &it:mp){
+            int freq=it.second;
+            freqcount[freq]++;
+        }
+        for(int freq=1;freq<n;freq++){
+            int types=min(k/freq,freqcount[freq]);
+            k-=(types*freq);
+            uniquetype-=types;
+            if(k<=freq){
+                return uniquetype;
+            }
+        }
+        return 0;
+    }
+};
